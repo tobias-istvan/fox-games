033f20c65bc2c4cb31da3eaa54319c67da2d8e8e
Istvan Tobias <tobias.istvan@gmail.com>
2023-04-05 23:10:36 +1200

    Add the new version under a new route

---
+++ src/routes/v2/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { GameScene } from '$lib/GameScene';
+  import { onMount } from 'svelte';
+  let el: HTMLCanvasElement;
+  let scene: GameScene;
+
+  onMount(() => {
+    scene = new GameScene(el);
+  });
+</script>
+
+<svelte:head>
+  <title>Adventure</title>
+  <meta name="description" content="Adventure" />
+</svelte:head>
+
+<canvas bind:this={el} style="width: 100vw; height: 100vh;" />
